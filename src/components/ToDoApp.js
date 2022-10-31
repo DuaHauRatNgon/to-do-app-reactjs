d35966705b16c9e039545af906c84056857da76c
DuaHauRatNgon <nmhuongbn@gmail.com>
2022-10-31 21:12:32 +0700

    app to do dang cap vu tru fetch api

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
 import "./ToDoApp.css"
 import Header from "./Header";
 import AddToDo from "./AddToDo";
@@ -7,73 +8,69 @@ import ToDos from "./ToDos";
 
 class ToDoApp extends React.Component {
     state = {
-        todos: [
-            {
-                id: 1,
-                title: "Setup development environment",
-                completed: true
-            },
-            {
-                id: 2,
-                title: "Develop website and add content",
-                completed: false
-            },
-            {
-                id: 3,
-                title: "Deploy to live server",
-                completed: false
-            }
-        ]
+        todos: []
     };
 
     handleAddToDo = (todoTitle) => {
-        let todoTmp = {
-            id: uuidv4(),
+        const todoData = {
             title: todoTitle,
             completed: false
-        };
-        this.setState({
-            todos: [...this.state.todos, todoTmp]
-        })
+        }
+        axios.post("https://jsonplaceholder.typicode.com/todos", todoData)
+            .then(response => {
+                console.log(response.data)
+                this.setState({
+                    todos: [...this.state.todos, response.data]
+                })
+            });
     }
 
     handleDoneTodo = (id) => {
         // console.log(id);
         this.setState({
             todos: this.state.todos.map((item) => {
-                if(item.id === id) {
+                if (item.id === id) {
                     item.completed = !item.completed;
                 }
                 return item;
             })
         });
-        // this.state.todos.map(item => {
-        //     console.log(item.completed);
-        // })
     };
 
     handleDeleteTodo = (id) => {
-        console.log(id);
-        this.setState({
-            todos: [...this.state.todos.filter((item) => {
-                if(item.id !== id) {
-                    return item;
-                }
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(reponse => {
+                this.setState({
+                    todos: [
+                        ...this.state.todos.filter(todo => {
+                            return todo.id !== id;
+                        })
+                    ]
+                })
             })
-        ]
-        });
+
     }
 
-    render(){
-        return(
+    componentDidMount() {
+        const config = {
+            params: {
+                _limit: 10
+            }
+        }
+        axios.get("https://jsonplaceholder.typicode.com/todos", config)
+            .then(response => this.setState({ todos: response.data }))
+    }
+
+    render() {
+        return (
             <div>
                 <div className="ToDoApp">
-                    <Header/>
-                    <AddToDo handleAddToDo = {this.handleAddToDo}/>
-                    <ToDos todos = {this.state.todos}
-                        handleDoneTodo = {this.handleDoneTodo}
-                        handleDeleteTodo = {this.handleDeleteTodo}
-                        />
+                    <Header />
+                    <AddToDo handleAddToDo={this.handleAddToDo} />
+                    <ToDos todos={this.state.todos}
+                        handleDoneTodo={this.handleDoneTodo}
+                        handleDeleteTodo={this.handleDeleteTodo}
+                    />
                 </div>
             </div>
         )
